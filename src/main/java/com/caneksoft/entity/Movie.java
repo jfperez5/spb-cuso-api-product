200fc8b4bc3631a19a6593e60c9c41c2e6273ca7
Josué Pérez <jfp.perez5@gmail.com>
2025-07-16 16:29:34 -0600

    no se estaban guardados los cambios de la clase movie

--- src/main/java/com/caneksoft/entity/Movie.java
+++ src/main/java/com/caneksoft/entity/Movie.java
@@ -1,5 +1,13 @@
 package com.caneksoft.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity 
+@Data
+
+@Table(name="movies")
 public class Movie {
 
 }
