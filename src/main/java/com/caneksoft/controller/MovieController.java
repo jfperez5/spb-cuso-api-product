de88180ed95d823dae8e0acd168ecd83f9524041
jfperez5 <65435098+jfperez5@users.noreply.github.com>
2025-07-17 05:56:40 -0600

    Resolviendo errores ya que no guarda los datos

---
+++ src/main/java/com/caneksoft/controller/MovieController.java
@@ -0,0 +1,86 @@
+package com.caneksoft.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.caneksoft.entity.Movie;
+import com.caneksoft.repository.MovieRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+    
+    @CrossOrigin
+    @GetMapping
+    public List<Movie>getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id){
+        Optional<Movie>movie = movieRepository.findById(id);
+        return movie.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
+    }    
+
+    @CrossOrigin
+    @PostMapping
+    public ResponseEntity<Movie>createMovie(@RequestBody Movie movie){
+        System.out.println("Recibido:");
+        System.out.println(movie);
+        Movie savedMovie = movieRepository.save(movie);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(savedMovie);
+    }    
+
+
+     @CrossOrigin
+     @DeleteMapping("/{id}")
+     public ResponseEntity<Void> deleteMovie(@PathVariable Long id){
+        if(!movieRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+       
+        movieRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+     }   
+
+     @CrossOrigin
+     @PutMapping("/{id}")
+     public ResponseEntity<Movie>updateMovie(@PathVariable Long id, @RequestBody Movie updatedMovie){
+        if(!movieRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+
+        updatedMovie.setId(id);
+        Movie savedMove = movieRepository.save(updatedMovie);
+        return ResponseEntity.ok(savedMove);
+     }
+
+
+
+
+
+
+
+}
+
+
+// @CrossOrigin se establece para indicar a Spring Boot que este metodo se podrá usar desde sistema fuera de spring ejm: React, Vue, Angular, etc.
--- src/main/java/com/caneksoft/entity/Movie.java
+++ src/main/java/com/caneksoft/entity/Movie.java
@@ -1,13 +1,118 @@
 package com.caneksoft.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity 
-@Data
-
 @Table(name="movies")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String year;
+    private String votes;
+    private double rating;
+    
+    @JsonProperty("imagenUrl")
+    @Column(name="imagen_url")
+    private String imagenUrl;
+
+
+    public Movie(Long id, String title, String year, String votes, double rating, String imagenUrl) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.votes = votes;
+        this.rating = rating;
+        this.imagenUrl = imagenUrl;
+    }
+
+
+    public Movie() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getYear() {
+        return year;
+    }
+
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+
+    public String getVotes() {
+        return votes;
+    }
+
+
+    public void setVotes(String votes) {
+        this.votes = votes;
+    }
+
+
+    public double getRating() {
+        return rating;
+    }
+
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Movie [id=" + id + ", title=" + title + ", year=" + year + ", votes=" + votes + ", rating=" + rating
+                + ", imagenUrl=" + imagenUrl + "]";
+    }
+
+    
+
+
+
 
 }
+
+
+// Dominando API REST con Spring Boot. Taller Completo | GOGODEV     3:16:39
+// by GOGODEV
